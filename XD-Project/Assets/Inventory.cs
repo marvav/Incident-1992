0344c8ffcf4b557f4f5f483128e4034730f6cb8b
Marek Horsky <xhorsky@fi.muni.cz>
2023-02-24 21:39:21 +0100

    Inventory tiles update

--- XD-Project/Assets/Inventory.cs
+++ XD-Project/Assets/Inventory.cs
@@ -10,9 +10,11 @@ public class InventoryMenu : MonoBehaviour
     public GameObject[] InventoryUI;
     public GameObject Inventory_Object;
     private InventoryAPI.Inventory Inventory;
+    private List<Vector3> Icon_positions;
     // Start is called before the first frame update
     void Start()
     {
+        Icon_positions = new List<Vector3> {new Vector3(-435, 180, 0), new Vector3(-315, 180, 0) };
         Inventory = Inventory_Object.GetComponent<InventoryItems>().Inventory;
     }
 
@@ -36,7 +38,7 @@ public class InventoryMenu : MonoBehaviour
     void ToggleOn(GameObject[] objects)
     {
         Inventory_Object.SetActive(true);
-
+        int index = 0;
         foreach (GameObject element in objects)
         {
             if(element.tag == "Icon")
@@ -46,8 +48,15 @@ public class InventoryMenu : MonoBehaviour
                 {
                     element.SetActive(true);
                     TMP_Text Count;
-                    Count = element.transform.GetChild(0).GetComponent<TMP_Text>();
-                    Count.text = item.count.ToString();
+                    if (element.name == "Battery") //You can have more batteries, so there is the count
+                    {
+                        Count = element.transform.GetChild(0).GetComponent<TMP_Text>();
+                        Count.text = item.count.ToString();
+                    }
+
+                    RectTransform element_pos = element.GetComponent<RectTransform>();
+                    element_pos.anchoredPosition = Icon_positions[index];
+                    index++;
                 }
             }
             else
