7fbbae59220ad0b611a990e11bf84518d06f730d
Marek Horsky <xhorsky@fi.muni.cz>
2023-03-11 21:45:25 +0100

    Changed terrain scale

---
+++ Assets/ItemUse.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+using static Inventory;
+
+
+public class ItemUse : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject Description;
+    public GameObject ItemInHand;
+
+    //Detect if the Cursor starts to pass over the GameObject
+    void Start()
+    {
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        int clickCount = eventData.clickCount;
+
+        if (clickCount == 2)
+        {
+            Debug.Log("Item in hand");
+            Description.SetActive(false);
+            ItemInHand.SetActive(true);
+
+        }
+    }
+}
