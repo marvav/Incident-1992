4519ec56de15fbda1bf542f92bc0605fc277f09a
Šimon Vladík <simon.vladik@gmail.com>
2023-05-09 00:52:20 +0200

    eye edit, darkness edit, 3 spotlights make the flashlight look good?

---
+++ Assets/billBoard.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class billBoard : MonoBehaviour
+{
+    Camera mainCamera;
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+    void LateUpdate()
+    {
+        transform.LookAt(mainCamera.transform);
+        //transform.Rotate(0, 180, 0);
+    }
+}
