598446f4bdcca7672be399f2a20ac6f86869b727
Marek Horsky <xhorsky@fi.muni.cz>
2023-05-14 15:27:17 +0200

    Added some models, blinking lamps scripts, buzzing ambience

---
+++ Assets/BlinkingLight.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingLight : MonoBehaviour
+{
+    public int blinking_intensity;
+    public int intensity;
+    public System.Random rand;
+    private Light light;
+    private bool isFlickering;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+        rand = new System.Random();
+    }
+    void FixedUpdate()
+    {
+        if (!isFlickering && rand.Next(0, blinking_intensity) == 1)
+        {
+            isFlickering = true;
+        }
+        if (isFlickering)
+        {
+            int random = rand.Next(0, 5000);
+            if (random > 4500)
+            {
+                light.intensity = rand.Next(0, intensity);
+            }
+            if (random < 10)
+            {
+                isFlickering = false;
+            }
+        }
+    }
+}
