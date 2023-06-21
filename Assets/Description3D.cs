9afb4dbd572d94e54ea77499221523115c27520a
marvav <marek.horsk@seznam.cz>
2023-06-21 15:20:50 +0200

    Electric Poles alongside road

--- Assets/Description3D.cs
+++ Assets/Description3D.cs
@@ -8,13 +8,12 @@ public class Description3D : MonoBehaviour
     public Core Core;
     public string text;
     public float range = 2.0f;
-    public float angle = 90.0f;
     public int clueID = 0;
     private bool isHidden = true;
 
     void Update()
     {
-        if (CanInteract(this.gameObject, range, angle))
+        if (isCloseToPlayer(transform, range))
         {
             if (isHidden)
             {
