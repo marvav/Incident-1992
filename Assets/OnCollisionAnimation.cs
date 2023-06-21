0165e56388c88d0052f4fbff131da853e32716c2
marvav <marek.horsk@seznam.cz>
2023-06-21 17:28:25 +0200

    crying over rigidbody

--- Assets/OnCollisionAnimation.cs
+++ Assets/OnCollisionAnimation.cs
@@ -4,19 +4,37 @@ using UnityEngine;
 
 public class OnCollisionAnimation : MonoBehaviour
 {
-    public Animation Animation;
+    public GameObject Gate;
+    public int initial_rotation;
+    public int desired_rotation;
+    private bool play = true;
+    void OnEnable()
+    {
+        play = true;
+        Quaternion target = Quaternion.Euler(0, initial_rotation, 0);
+        Gate.transform.rotation = Quaternion.Slerp(Gate.transform.rotation, target, Time.deltaTime);
+    }
+    void Update()
+    {
+        if (play)
+        {
+            if (Gate.transform.rotation.y > desired_rotation)
+            {
+                Quaternion target = Quaternion.Euler(0, -Time.deltaTime, 0);
+
+                // Dampen towards the target rotation
+                Gate.transform.rotation = Quaternion.Slerp(Gate.transform.rotation, target, Time.deltaTime);
+            }
+            else
+                play = false;
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("collision");
-        if (Animation.isPlaying && collision.gameObject.tag == "Pickable")
+        if (collision.gameObject.layer == 7)
         {
-            Animation.Stop();
-            //Animation[Animation.clip].speed = 0;
+            play = false;
         }
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        Debug.Log("collision ended");
     }
 }
