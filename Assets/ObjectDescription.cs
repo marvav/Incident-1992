af81158580069400d19c3beda9762888bab36c0c
marvav <41584620+marvav@users.noreply.github.com>
2023-07-09 18:30:50 +0200

    Update ObjectDescription.cs

--- Assets/ObjectDescription.cs
+++ Assets/ObjectDescription.cs
@@ -5,31 +5,10 @@ using static Core_Utils;
 
 public class ObjectDescription : MonoBehaviour
 {
-    public Core Core;
     public string[] contents;
-    public int clueID;
-    private bool isHidden = true;
 
-    // Update is called once per frame
-    void Update()
+    public string GetText(int index)
     {
-        if (CanInteract(this.gameObject, 1.0f, 35.0f))
-        {
-            if (isHidden)
-            {
-                Core.Description.text = contents[Core.GetLanguage()];
-                Core.ProgressManager.changeObjective(clueID);
-                isHidden = false;
-            }
-        }
-        else
-        {
-            if (!isHidden)
-            {
-                Core.Description.text = "";
-                Core.PickUpItem.SetActive(false);
-                isHidden = true;
-            }
-        }
+        return contents[index];
     }
 }
